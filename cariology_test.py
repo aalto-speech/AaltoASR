284d7acbbb193677d38c6d027803342f4f83ae0d
Seppo Enarvi <seppo.enarvi@aalto.fi>
2011-07-27 14:53:38 +0000

    Modified for status dictation. Still not working.

--- cariology_test.py
+++ cariology_test.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 #
-# Recognizes test commands for cariological status dictation. Takes the
-# training data directory as an argument, e.g.
-#   ./cariology_test.py ~/DentistryData
+# Recognizes cariological status dictation, or free dictation (select using the
+# lm variable). Takes the model directory, speech directory, and output file as
+# arguments, e.g.
+#   ./cariology_test.py /share/puhe/hammas /share/puhe/audio/hammas2011/andre050711 output.csv
 
 import time
 import string
@@ -19,6 +20,30 @@ import math
 sys.path.append(os.path.dirname(sys.argv[0]) + "/src/swig");
 
 import Decoder
+
+##################################################
+# From Wikibooks
+# http://en.wikibooks.org/wiki/Algorithm_implementation/Strings/Levenshtein_distance#Python
+#
+
+def levenshtein(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+    if not s1:
+        return len(s2)
+ 
+    previous_row = xrange(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+ 
+    return previous_row[-1]
+
 
 ##################################################
 # Initialize
@@ -39,18 +64,18 @@ dur = ac_model + ".dur"
 
 speech_directory = sys.argv[2]
 
-# language model for cariology status commands
-#lexicon = sys.argv[1] + "/CariologyLexicon.lex"
-#ngram = sys.argv[1] + "/CariologyLM.even.3gram.bin"
-#lookahead_ngram = sys.argv[1] + "/CariologyLM.even.2gram.bin"
-
+#lm = 'FreeDictation'
+lm = 'StatusDictation'
 #lm = ''
-lm = 'FreeDictation'
 
 if lm == 'FreeDictation':
 	lexicon = "/share/puhe/hammas/FreeDictationLexicon.lex"
 	ngram = "/share/puhe/hammas/FreeDictationLM.6gram.bin"
 	lookahead_ngram = "/share/puhe/hammas/FreeDictationLM.2gram.bin"
+elif lm == 'StatusDictation':
+	lexicon = "/share/puhe/hammas/StatusDictationLexicon.lex"
+	ngram = "/share/puhe/hammas/StatusDictationLM.even.3gram.bin"
+	lookahead_ngram = "/share/puhe/hammas/StatusDictationLM.even.2gram.bin"
 else:
 	lexicon = "/share/work/jpylkkon/bin_lm/morph19k.lex"
 	ngram = "/share/work/jpylkkon/bin_lm/morph19k_D20E10_varigram.bin"
@@ -107,13 +132,13 @@ t = Decoder.Toolbox()
 
 t.select_decoder(0)
 
-# Emable for morph models.
-t.set_silence_is_word(1)
+if lm != 'StatusDictation':
+	# Emable for morph models.
+	t.set_silence_is_word(1)
 
 t.set_optional_short_silence(1)
 
 t.set_cross_word_triphones(1)
-# t.set_require_sentence_end(0)
 t.set_require_sentence_end(1)
 
 print "Loading acoustic model."
@@ -129,8 +154,9 @@ t.set_generate_word_graph(1)
 
 t.set_lm_lookahead(1)
 
-# Emable for morph models.
-t.set_word_boundary("<w>")
+if lm != 'StatusDictation':
+	# Emable for morph models.
+	t.set_word_boundary("<w>")
 
 print "Loading lexicon."
 try:
@@ -168,10 +194,14 @@ t.set_lm_scale(lm_scale)
 # t.set_insertion_penalty(-0.5)
 
 print "Recognizing audio files."
+output_file = open(sys.argv[3], "w")
 for lna_file in os.listdir(speech_directory):
 	if not lna_file.endswith('.lna'):
 		continue
-	print ":: " + lna_file[:-4]
+	
+	name = lna_file[:-4]
+	print name
+	
 	lna_path = speech_directory + "/" + lna_file
 	rec_path = lna_path[:-4] + ".rec"
 	txt_path = lna_path[:-4] + ".txt"
@@ -179,44 +209,41 @@ for lna_file in os.listdir(speech_directory):
 	t.lna_open(lna_path, 1024)
 	t.reset(0)
 	t.set_end(-1)
-	while (True):
-		if (not t.run()):
-			# We have to open with only "w" first, and then later with "r"
-			# for reading, or the file will not be written.
-			rec = open(rec_path, "w")
-			t.print_best_lm_history_to_file(rec)
-			t.write_word_graph(slf_path);
-			rec.close()
-			
-			command = 'lattice-tool -read-htk -in-lattice "' + \
-					slf_path + \
-					'" -nbest-decode 100 -out-nbest-dir "' + \
-					speech_directory + '"'
-			return_value = os.system(command)
-			if return_value != 0:
-				print "Command returned a non-zero exit status: ", command
-				sys.exit(1)
-    		
-			nbest_file = gzip.open(slf_path + ".gz", "rb")
-			nbest_list = nbest_file.readlines()
-			nbest_file.close()
+	while t.run():
+		pass
+	
+	# We have to open with only "w" first, and then later with "r"
+	# for reading, or the file will not be written.
+	rec = open(rec_path, "w")
+	t.print_best_lm_history_to_file(rec)
+	t.write_word_graph(slf_path);
+	rec.close()
+	
+	command = 'lattice-tool -read-htk -in-lattice "' + \
+			slf_path + \
+			'" -nbest-decode 100 -out-nbest-dir "' + \
+			speech_directory + '"'
+	return_value = os.system(command)
+	if return_value != 0:
+		print "Command returned a non-zero exit status: ", command
+		sys.exit(1)
+	
+	nbest_file = gzip.open(slf_path + ".gz", "rb")
+	nbest_list = nbest_file.readlines()
+	nbest_file.close()
 
-			rec = open(rec_path, "r")
-			recognition = rec.read().strip()
-			rec.close()
-			break
+	rec = open(rec_path, "r")
+	recognition = rec.read()
+	recognition = recognition.replace(' ', '')
+	recognition = recognition.replace('<s>', '')
+	recognition = recognition.replace('</s>', '')
+	recognition = recognition.replace('<w>', ' ')
+	recognition = recognition.strip()
+	rec.close()
 
-	if os.path.exists(txt_path):
-		equal = filecmp.cmp(rec_path, txt_path)
-		if equal:
-			print "OK ", recognition
-		else:
-			txt = open(txt_path, "r")
-			transcription = txt.read().strip()
-			txt.close()
-			print "F ", recognition, " != ", transcription
-	else:
-		print "? ", recognition
+	rec = open(rec_path, "w")
+	rec.write(recognition)
+	rec.close()
 
 	# Compensate for incorrect assumptions in the HMM by flattening the
 	# logprobs.
@@ -231,4 +258,20 @@ for lna_file in os.listdir(speech_directory):
 		# Acoustic probabilities are calculated in natural logarithm space.
 		total_logprob += math.log(1 + math.exp(logprob - total_logprob))
 	
-	print str(logprob_1 - total_logprob)
+	log_confidence = logprob_1 - total_logprob
+
+	if os.path.exists(txt_path):
+		txt = open(txt_path, "r")
+		transcription = txt.read().strip()
+		txt.close()
+		distance = levenshtein(recognition, transcription)
+		num_letters = len(transcription)
+		line = '{0}\t{1}\t{2}\t{3}\t{4}\t{5:.4}\n'.format(name, transcription, recognition, distance, num_letters, log_confidence)
+		output_file.write(line)
+		output_file.flush()
+	else:
+		print 'No transcription.'
+		print 'Recognition result:', recognition
+		print 'Log confidence:', log_confidence
+
+output_file.close()
