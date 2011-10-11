081e4dfd513c695d6b858dfaeeacdebd1c100112
Seppo Enarvi <seppo.enarvi@aalto.fi>
2011-10-11 16:07:14 +0000

    -q (quiet) option.

--- lattice_rescore.cc
+++ lattice_rescore.cc
@@ -63,6 +63,7 @@ main(int argc, char *argv[])
     ('O', "out-dir=DIR", "arg", "", "output directory")
     ('p', "post-process=FILE", "arg", "", 
      "run a post-processor for each output file")
+    ('q', "quiet", "", "", "suppress all output on standard error")
     ;
   config.parse(argc, argv);
   if (config["help"].specified) {
@@ -72,26 +73,32 @@ main(int argc, char *argv[])
   if (config["config"].specified)
     config.read(io::Stream(config["config"].get_str(), "r").file);
 
+  bool quiet = config["quiet"].specified;
   if (config.arguments.size() != 0) {
-    fputs(config.help_string().c_str(), stderr);
+    if (!quiet)
+      fputs(config.help_string().c_str(), stderr);
     exit(1);
   }
 
   // Read the language model
-  fprintf(stderr, "reading the language model...");
+  if (!quiet)
+    fprintf(stderr, "reading the language model...");
   TreeGram tree_gram;
   if (config["lm"].specified)
     tree_gram.read(io::Stream(config["lm"].get_str(), "r").file);
-  fprintf(stderr, "\n");
+  if (!quiet)
+    fprintf(stderr, "\n");
 
   // Parse input lattices
   std::vector<std::string> input_files;
   if (config["in"].specified && config["in-list"].specified) {
-    fprintf(stderr, "ERROR: do not specify input lattice and input list\n");
+    if (!quiet)
+      fprintf(stderr, "ERROR: do not specify input lattice and input list\n");
     exit(1);
   }
   if (!config["in"].specified && !config["in-list"].specified) {
-    fprintf(stderr, "ERROR: must specify input lattice or input list\n");
+	if (!quiet)
+	  fprintf(stderr, "ERROR: must specify input lattice or input list\n");
     exit(1);
   }
   if (config["in"].specified)
@@ -115,24 +122,29 @@ main(int argc, char *argv[])
       output_file = 
         config["out-dir"].get_str() + "/" + strip_dir(input_files[i]);
     if (file_exists(output_file) && !config["force"].specified) {
-      fprintf(stderr, "skipped existing file %s\n", output_file.c_str());
+      if (!quiet)
+        fprintf(stderr, "skipped existing file %s\n", output_file.c_str());
       continue;
     }
 
-    fprintf(stderr, "processing %s...", input_files[i].c_str());
+    if (!quiet)
+      fprintf(stderr, "processing %s...", input_files[i].c_str());
     src_lattice.read(io::Stream(input_files[i], "r").file);
     rescore.rescore(&src_lattice, &tree_gram);
 
-    fprintf(stderr, "writing %s...", output_file.c_str());
+    if (!quiet)
+      fprintf(stderr, "writing %s...", output_file.c_str());
     rescore.rescored_lattice().write(io::Stream(output_file, "w").file);
-    fprintf(stderr, "\n");
+    if (!quiet)
+      fprintf(stderr, "\n");
 
     if (config["post-process"].specified) {
       std::string cmd = config["post-process"].get_str() + 
         " \"" + output_file + "\"";
-      fprintf(stderr, "running post-processor: %s\n", cmd.c_str());
+      if (!quiet)
+        fprintf(stderr, "running post-processor: %s\n", cmd.c_str());
       int ret = system(cmd.c_str());
-      if (ret < 0) {
+      if ((ret < 0) && !quiet) {
         fprintf(stderr, "WARNING: command failed\n");
       }
     }
