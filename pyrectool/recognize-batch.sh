009cee63b952afa1dd53b0eef4666bd1c8d1c505
Seppo Enarvi <seppo.git@marjaniemi.com>
2015-12-15 22:27:54 +0200

    Possibility to specify output file.

--- pyrectool/recognize-batch.sh
+++ pyrectool/recognize-batch.sh
@@ -8,154 +8,161 @@
 #   $WORK_DIR/results
 # with a name unique to selected options.
 
-SCRIPT_DIR=$(dirname "$0")
-SORT_TRN="$SCRIPT_DIR/../scoring-scripts/sort-trn.py"
+script_dir=$(readlink -f "$(dirname $0)")
+sort_trn="${script_dir}/../scoring-scripts/sort-trn.py"
 
-[ -n "$AM" ] || { echo "AM environment variable needs to be specified." >&2; exit 2; }
-[ -n "$LM" ] || { echo "LM environment variable needs to be specified." >&2; exit 2; }
-[ -n "$DICTIONARY" ] || { echo "DICTIONARY environment variable needs to be specified." >&2; exit 2; }
-[ -n "$NUM_BATCHES" ] || NUM_BATCHES=1
-[ -n "$MAX_PARALLEL" ] || MAX_PARALLEL=1
-[ -n "$BEAM" ] || BEAM=280
-[ -n "$LM_SCALE" ] || LM_SCALE=30
-[ -n "$TOKEN_LIMIT" ] || TOKEN_LIMIT=100000
-[ -n "$RECOGNITIONS_DIR" ] || RECOGNITIONS_DIR="$WORK_DIR/recognitions"
-[ -n "$RESULTS_DIR" ] || RESULTS_DIR="$WORK_DIR/results"
+[ -n "${AM}" ] || { echo "AM environment variable needs to be specified." >&2; exit 2; }
+[ -n "${LM}" ] || { echo "LM environment variable needs to be specified." >&2; exit 2; }
+[ -n "${DICTIONARY}" ] || { echo "DICTIONARY environment variable needs to be specified." >&2; exit 2; }
+[ -n "${NUM_BATCHES}" ] || NUM_BATCHES=1
+[ -n "${MAX_PARALLEL}" ] || MAX_PARALLEL=1
+[ -n "${BEAM}" ] || BEAM=280
+[ -n "${LM_SCALE}" ] || LM_SCALE=30
+[ -n "${TOKEN_LIMIT}" ] || TOKEN_LIMIT=100000
+[ -n "${RECOGNITIONS_DIR}" ] || RECOGNITIONS_DIR="${WORK_DIR}/recognitions"
 
-AM_OPT=$(basename $AM)
+am_opt="$(basename ${AM})"
 
-DECODER_OPT=""
-[ -n "$DECODER_VER" ] && DECODER_OPT="$DECODER_VER--"
-DECODER_OPT="$DECODER_OPT$(basename $LM)"
+decoder_opt=""
+[ -n "${DECODER_VER}" ] && decoder_opt="${DECODER_VER}--"
+decoder_opt="${decoder_opt}$(basename $LM)"
 
-PARAMS=""
+declare -a params
+params=()
 
-[ -n "$AKU" ] && PARAMS="$PARAMS --aku $AKU"
-[ -n "$DECODER" ] && PARAMS="$PARAMS --decoder $DECODER"
+[ -n "${AKU}" ] && params+=(--aku "${AKU}")
+[ -n "${DECODER}" ] && params+=(--decoder "${DECODER}")
 
 # Specify binary LM if .bin or .fsabin file exists.
-if [ "$FSA" != "" ]
+if [ -n "${FSA}" ]
 then
-	PARAMS="$PARAMS --fsa"
-	BIN_LM_PATH="$LM.fsabin"
-	DECODER_OPT=$DECODER_OPT-fsa
+	params+=(--fsa)
+	bin_lm_path="${LM}.fsabin"
+	decoder_opt=${decoder_opt}-fsa
 else
-	BIN_LM_PATH="$LM.bin"
+	bin_lm_path="${LM}.bin"
 fi
-if [ -e "$BIN_LM_PATH" ]
+if [ -e "${bin_lm_path}" ]
 then
-	PARAMS="$PARAMS --bin-lm $BIN_LM_PATH"
-elif [ ! -e "$LM" ]
+	params+=(--bin-lm "${bin_lm_path}")
+elif [ ! -e "${LM}" ]
 then
-	echo "Neither binary LM ($BIN_LM_PATH) nor ARPA LM ($LM) was found." >&2
+	echo "Neither binary LM (${bin_lm_path}) nor ARPA LM (${LM}) was found." >&2
 	exit 2
 fi
 
 # ARPA LM may be needed in any case for rescoring lattices.
-if [ -e "$LM" ]
+if [ -e "${LM}" ]
 then
-	PARAMS="$PARAMS --arpa-lm $LM"
+	params+=(--arpa-lm "${LM}")
 fi
 
 # Specify binary lookahead LM if exists, otherwise ARPA.
-if [ "$LOOKAHEAD_LM" != "" ]
+if [ -n "${LOOKAHEAD_LM}" ]
 then
-	if [ -e "$LOOKAHEAD_LM.bin" ]
+	if [ -e "${LOOKAHEAD_LM}.bin" ]
 	then
-		PARAMS="$PARAMS --lookahead-bin-lm $LOOKAHEAD_LM.bin"
-	elif [ -e "$LOOKAHEAD_LM" ]
+		params+=(--lookahead-bin-lm "${LOOKAHEAD_LM}.bin")
+	elif [ -e "${LOOKAHEAD_LM}" ]
 	then
-		PARAMS="$PARAMS --lookahead-arpa-lm $LOOKAHEAD_LM"
+		params+=(--lookahead-arpa-lm "${LOOKAHEAD_LM}")
 	else
-		echo "Lookahead LM does not exist: $LOOKAHEAD_LM" >&2
+		echo "Lookahead LM does not exist: ${LOOKAHEAD_LM}" >&2
 		exit 2
 	fi
 fi
 
-if [ "$CLASSES" != "" ]
+if [ -n "${CLASSES}" ]
 then
-	PARAMS="$PARAMS --classes $CLASSES"
+	params+=(--classes "${CLASSES}")
 fi
 
-if [ "$SPLIT_MULTIWORDS" != "" ]
+if [ -n "${SPLIT_MULTIWORDS}" ]
 then
-	PARAMS="$PARAMS --split-multiwords"
-	DECODER_OPT=$DECODER_OPT-mw
+	params+=(--split-multiwords)
+	decoder_opt="${decoder_opt}-mw"
 fi
 
-if [ "$GENERATE_LATTICES" != "" ]
+if [ -n "${GENERATE_LATTICES}" ]
 then
-	PARAMS="$PARAMS --generate-word-graph"
-	if [ "$LATTICE_TOOL" = "" ]
+	params+=(--generate-word-graph)
+	if [ "${LATTICE_TOOL}" = "" ]
 	then
 		LATTICE_TOOL=$(which lattice-tool)
 	fi
-	if [ "$LATTICE_TOOL" != "" ]
+	if [ -n "${LATTICE_TOOL}" ]
 	then
-		PARAMS="$PARAMS --lattice-tool $LATTICE_TOOL"
+		params+=(--lattice-tool "${LATTICE_TOOL}")
 	fi
-	if [ "$LATTICE_RESCORE" != "" ]
+	if [ -n "${LATTICE_RESCORE}" ]
 	then
-		PARAMS="$PARAMS --lattice-rescore $LATTICE_RESCORE"
+		params+=(--lattice-rescore "${LATTICE_RESCORE}")
 	fi
-	if [ "$WORD_PAIR_APPROXIMATION" != "" ]
+	if [ -n "${WORD_PAIR_APPROXIMATION}" ]
 	then
-		PARAMS="$PARAMS --word-pair-approximation"
-		DECODER_OPT=$DECODER_OPT-wpa
+		params+=(--word-pair-approximation)
+		decoder_opt="${decoder_opt}-wpa"
 	fi
 fi
 
-DECODER_OPT=$DECODER_OPT-b$BEAM-s$LM_SCALE
-DECODER_OPT=$DECODER_OPT-tl$(echo $TOKEN_LIMIT|"$SCRIPT_DIR/human-readable.awk")
+tl_string=$(echo "${TOKEN_LIMIT}" | "${script_dir}/human-readable.awk")
+decoder_opt="${decoder_opt}-b${BEAM}-s${LM_SCALE}-tl${tl_string}"
 
-if [ "$ADAPTATION" != "" ]
+if [ -n "${ADAPTATION}" ]
 then
-	PARAMS="$PARAMS --adapt $ADAPTATION"
-	DECODER_OPT="$DECODER_OPT-$ADAPTATION"
-	if [ "$SPEAKER_ID_FIELD" = "" ]
+	params+=(--adapt "${ADAPTATION}")
+	decoder_opt="${decoder_opt}-${ADAPTATION}"
+	if [ "${SPEAKER_ID_FIELD}" = "" ]
 	then
 		echo "Warning: speaker ID field not specified. Using default (3)." 2>&1
-		PARAMS="$PARAMS --speaker-id-field 3"
+		params+=(--speaker-id-field 3)
 	else
-		PARAMS="$PARAMS --speaker-id-field $SPEAKER_ID_FIELD"
+		params+=(--speaker-id-field "${SPEAKER_ID_FIELD}")
 	fi
 fi
 
-WORK_DIR="$RECOGNITIONS_DIR/$AM_OPT"
-REC_DIR="$WORK_DIR/$DECODER_OPT"
-HYP_FILE="$RESULTS_DIR/$AM_OPT--$DECODER_OPT.trn"
+work_dir="${RECOGNITIONS_DIR}/${am_opt}"
+rec_dir="${work_dir}/${decoder_opt}"
 
-mkdir -p "$REC_DIR"
-mkdir -p "$RESULTS_DIR"
-
-PARAMS="$PARAMS --am $AM"
-PARAMS="$PARAMS --dictionary $DICTIONARY.lex"
-PARAMS="$PARAMS --beam $BEAM"
-PARAMS="$PARAMS --language-model-scale $LM_SCALE"
-PARAMS="$PARAMS --token-limit $TOKEN_LIMIT"
-PARAMS="$PARAMS --hypothesis-file $HYP_FILE"
-PARAMS="$PARAMS --work-directory $WORK_DIR"
-PARAMS="$PARAMS --rec-directory $REC_DIR"
-PARAMS="$PARAMS -f $AUDIO_LIST"
-
-if [ $NUM_BATCHES -gt 1 ]
+if [ -n "${RESULTS}" ]
 then
-	"$SCRIPT_DIR/recognize-parallel.py" $PARAMS -B $NUM_BATCHES -P $MAX_PARALLEL
+	hyp_file="${RESULTS}"
 else
-	"$SCRIPT_DIR/recognize.py" $PARAMS
+	hyp_dir="${RESULTS_DIR:-${work_dir}/results}"
+	hyp_file="${hyp_dir}/${am_opt}--${decoder_opt}.trn"
+	mkdir -p "${hyp_dir}"
 fi
 
-EXIT_STATUS=$?
-if [ $EXIT_STATUS -ne 0 ]
+mkdir -p "${rec_dir}"
+
+params+=(--am "${AM}")
+params+=(--dictionary "${DICTIONARY}.lex")
+params+=(--beam "${BEAM}")
+params+=(--language-model-scale "${LM_SCALE}")
+params+=(--token-limit "${TOKEN_LIMIT}")
+params+=(--hypothesis-file "${hyp_file}")
+params+=(--work-directory "${work_dir}")
+params+=(--rec-directory "${rec_dir}")
+params+=(-f "${AUDIO_LIST}")
+
+if [ ${NUM_BATCHES} -gt 1 ]
 then
-	echo "Exit status: $EXIT_STATUS" >&2
-	exit $EXIT_STATUS
+	"${script_dir}/recognize-parallel.py" "${params[@]}" -B "${NUM_BATCHES}" -P "${MAX_PARALLEL}"
+else
+	"${script_dir}/recognize.py" "${params[@]}"
 fi
 
-if [ -x "$SORT_TRN" ]
+exit_status=${?}
+if [ ${exit_status} -ne 0 ]
 then
-	"$SORT_TRN" "$HYP_FILE"
+	echo "Exit status: ${exit_status}" >&2
+	exit ${exit_status}
 fi
 
-echo "Wrote $HYP_FILE."
+if [ -x "${sort_trn}" ]
+then
+	"${sort_trn}" "${hyp_file}"
+fi
+
+echo "Wrote ${hyp_file}."
 exit 0
